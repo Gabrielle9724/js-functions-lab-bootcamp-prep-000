e70374a2420ed02bd1a43e94e7428479876dcb04
Gabrielle Piacentile <gabriellepiacentile98@gmail.com>
2020-12-07 18:40:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,18 +5,18 @@ function happyHolidays(string){
   return "Happy holidays!"
 }
 
-function happyHolidaysTo(string){
+function happyHolidaysTo(name){
   var name = Gabby
   console.log("`Happy holidays, ${name}!`")
 }
 
-function happyCustomHolidayTo(string){
+function happyCustomHolidayTo(holiday, name){
     var holiday = birthday
   var name = Gabby
   console.log("`Happy ${holiday}` to ${name}!`")
 }
 
-function holidayCountdown(string){
+function holidayCountdown(holiday, days){
     var holiday = birthday
     var days = 4
   console.log("`It's ${days} days to ${holiday}`")
